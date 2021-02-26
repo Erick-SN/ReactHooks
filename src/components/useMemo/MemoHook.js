9ea9c73bb177ccf92aa176c076155a6493c6e447
Erick-SN <ericksn0707@gmail.com>
2021-02-25 18:32:29 -0600

    useMemo

---
+++ src/components/useMemo/MemoHook.js
@@ -0,0 +1,33 @@
+import { useCounter } from '../useState/AdvanceExample/useCounter';
+import { useState, useMemo } from 'react';
+
+const MemoHook = () => {
+  const { state, adding } = useCounter(11);
+  const [memo, setMemo] = useState(true);
+  const procesoPesado = (iteraciones) => {
+    for (let i = 0; i < iteraciones; i++) {
+      console.log('ahi vamos');
+    }
+    return `${iteraciones} veces`;
+  };
+  const memoProcesoPesado = useMemo(() => procesoPesado(state), [state]);
+  return (
+    <>
+      <div>
+        <h1>
+          MemoHook:<small>{state}</small>
+        </h1>
+        <hr />
+        <p>{memoProcesoPesado}</p>
+        <button className="btn btn-primary" onClick={() => adding()}>
+          +1
+        </button>
+        <button className="btn btn-primary" onClick={setMemo(!memo)}>
+          Show/Hide {JSON.stringify(memo)}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default MemoHook;
